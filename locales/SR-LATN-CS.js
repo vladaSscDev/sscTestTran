af493f40a61800f95f8379e12582fbc1f5e45fe1
PhraseApp <support@phraseapp.com>
2018-03-02 09:41:58 +0100

    Created locale file SR-LATN-CS.js

---
+++ locales/SR-LATN-CS.js
@@ -0,0 +1,6 @@
+{
+  "Overriden form help": "Ovo je overridden pomoćni tekst forme",
+  "Some field help": "Pomoćni tekst za tekstualno polje",
+  "Some form help": "Pomoćni tekst forme",
+  "Some other field help": "Pomoćni tekst za brojno polje"
+}
